535568d81acf6cd520c1a22a370db7b6bc996bfe
Peter Seibel <peter@gigamonkeys.com>
2021-11-05 23:44:12 +0000

    Convert many functions to more functional style.

--- script.js
+++ script.js
@@ -1,108 +1,91 @@
-const ALPHABET = "abcdefghijklmnopqrstuvwxyz ,.?!;:";
-const MAX_GENERATIONS = 1000;
-
-function solve(phrase, populationSize = 1000, mutationRate = 0.0001, alphabet = ALPHABET) {
-  let generator = makeRandomPhraseGenerator(phrase.length, alphabet);
-  let fitness = makeFitnessFunction(phrase);
-  let crosser = cross;
-  let mutator = makeMutator(alphabet, mutationRate);
-  let done = makeStopTest(phrase);
+/*
+ * Top-level solver. This just sets up the various functions we need to pass
+ * into genetic_search and then calls it to do all the real work. In theory
+ * the basic structure of genetic_search should work for any problem as long
+ * as we can define the five functions generator, fitness, crosser, mutator,
+ * and done in some appropriate way.
+ */
+function solve(
+  phrase,
+  populationSize = 1000,
+  mutationRate = 0.0001,
+  maxGenerations = 1000,
+  alphabet = "abcdefghijklmnopqrstuvwxyz ,.?!;:"
+  )
+{
+  let generator = randomPhraseGenerator(phrase.length, alphabet);
+  let fitness = phraseSimilarity(phrase);
+  let crosser = randomCrossover;
+  let mutator = pointMutator(alphabet, mutationRate);
+  let done = fitnessThreshold(fitness, phrase.length);
 
   let population = newPopulation(populationSize, generator);
-  return genetic_search(population, fitness, crosser, mutator, done, 0);
+  return genetic_search(population, fitness, crosser, mutator, done, maxGenerations, 0);
 }
 
-function genetic_search(population, fitness, crosser, mutator, done, generation) {
-  if (generation == MAX_GENERATIONS || done(population)) {
+/*
+ * Recursive search. Most of the arguments to this function are other functions
+ * that provide the specific implementations of things like measuring fitness,
+ * crossing two parents, mutating the result, and testing the population to see
+ * if we are done. We also limit the number of generations so we don't recurse forever.
+ * 
+ * Because Javascript doesn't do tail-call optimimization, this implementation could
+ * eventually run into limits on recursion depth but modern browsers have pretty high
+ * limits (over 9,000 at least if Stack Overflow is to be trusted.)
+ */ 
+function genetic_search(population, fitness, crosser, mutator, done, maxGenerations, generation) {
+  if (generation == maxGenerations || done(population)) {
     return { best: best(population, fitness), generation: generation }
   } else {
     let parents = getParents(population, fitness);
     let next = nextGeneration(population.length, parents, crosser, mutator);
-    return genetic_search(next, fitness, crosser, mutator, done, generation + 1);
+    return genetic_search(next, fitness, crosser, mutator, done, maxGenerations, generation + 1);
   }
 }
 
-function makeRandomPhraseGenerator(length, alphabet) {
-  return () => {
-    let phrase = "";
-    for (let i = 0; i < length; i++) {
-      phrase += random(alphabet);
-    }
-    return phrase;
-  };
+function randomPhraseGenerator(length, alphabet) {
+  return () => Array(length).fill().map(() => random(alphabet)).join("");
 }
 
 function newPopulation(size, generator) {
-  let population = [];
-  for (let i = 0; i < size; i++) {
-    population.push(generator());
-  }
-  return population;
+  return Array(size).fill().map(generator);
 }
 
-function makeFitnessFunction(phrase) {
-  return (element) => {
-    let f = 0;
-    for (let i = 0; i < element.length; i++) {
-      if (element[i] === phrase[i]) {
-        f++;
-      }
-    }
-    return f;
-  };
+function phraseSimilarity(phrase) {
+  return (p) => Array.from(p).reduce((acc, c, i) => acc + (c == phrase[i] ? 1 : 0), 0);
 }
 
-function makeMutator(alphabet, mutationRate) {
-  return (phrase) => {
-    let mutated = "";
-    for (let i = 0; i < phrase.length; i++) {
-      mutated += Math.random() < mutationRate ? random(alphabet) : phrase[i];
-    }
-    return mutated;
-  };
+function pointMutator(alphabet, mutationRate) {
+  let maybeMutate = (c) => Math.random() < mutationRate ? random(alphabet) : c;
+  return (p) => Array.from(p).map(maybeMutate).join("");
 }
 
-function makeStopTest(phrase) {
-  return (population) => population.indexOf(phrase) != -1;
+function fitnessThreshold(fitness, threshold) {
+  return (population) => population.some((p) => fitness(p) >= threshold);
 }
 
-function cross(p1, p2) {
+function randomCrossover(p1, p2) {
   let i = Math.floor(Math.random() * p1.length);
   return p1.substring(0, i) + p2.substring(i);
 }
 
 function best(population, fitness) {
-  let maxFitness = -1;
-  let best = null;
-  for (let i = 0; i < population.length; i++) {
-    let f = fitness(population[i]);
-    if (f > maxFitness) {
-      maxFitness = f;
-      best = population[i];
-    }
-  }
-  return best;
+  let wrap = (p) => ({ p: p, fitness: fitness(p) });
+  return population.map(wrap).reduce((best, p) => (p.fitness > best.fitness ? p : best)).p;
 }
 
 function getParents(population, fitness) {
-  let parents = [];
-  for (var i = 0; i < population.length; i++) {
-    for (let j = 0; j < fitness(population[i]); j++) {
-      parents.push(population[i]);
-    }
-  }
+  let parents = population.flatMap((p) => Array(fitness(p)).fill(p));
   // It's possible, though unlikely, that every member of the population
-  // has fitness zero and thus there are no parents. Gotta hope for some
-  // benefitial mutations in that case.
+  // has fitness zero and thus there are no parents. Everybody in the current
+  // generation gets a chance to procreate in that case and we have to hope
+  // for some benefitial mutations.
   return parents.length > 0 ? parents : population;
 }
 
 function nextGeneration(size, parents, crosser, mutator) {
-  let gen = [];
-  for (var i = 0; i < size; i++) {
-    gen.push(mutator(crosser(random(parents), random(parents))));
-  }
-  return gen;
+  let makeBaby = () => mutator(crosser(random(parents), random(parents)));
+  return Array(size).fill().map(makeBaby);
 }
 
 function random(xs) {
